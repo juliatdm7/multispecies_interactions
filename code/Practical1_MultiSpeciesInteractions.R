14bfa4c60e2d7b8cfa2e99e19e0cea0ab858abd0
juliatdm7 <juliatdm7@gmail.com>
2024-10-15 14:22:57 +0100

    Changes in parameters. Plotting the model.

--- code/Practical1_MultiSpeciesInteractions.R
+++ code/Practical1_MultiSpeciesInteractions.R
@@ -4,6 +4,8 @@
 #1.The predator-prey Lotka-Volterra model
 #Implementation of the base Lotka-Volterra model
 #To do so, we will use the ode() function from deSolve package
+graphics.off()
+
 
 library(deSolve)
 
@@ -50,4 +52,52 @@ ggplot(data = out.df)+
   geom_hline(yintercept=0,color="darkgrey") +
   geom_vline(xintercept=0,color="darkgrey") +
   labs(x = "Prey", y = "Predator")
+
+#Now, we can try and change our parameters, and see what happens.
+
+#What happens if we increase alfa (our prey population) quite a bit?... 
+state <- c(x=10,y=10) ## the initial population values for both x (prey) and y (predator) populations
+parameters <- c(alfa=0.5, ## alfa is the growth rate of the prey (x) population,
+                beta=0.02, ## beta is the effect of predator (y) on the prey (x) growth rate,
+                delta=0.02, ## gamma is the effect of prey (x) on the predator (y) growth rate
+                gamma=0.4)  ## and gamma is the predator (y) death rate.
+times <- seq(0,500,by=0.01) ##a sequence of time steps – uses function seq()
+out <- ode(y = state, times = times, func = LV, parms = parameters)
+out.df1 <- out.df
+out.df3 <- data.frame(out)
+out.df <- cbind(out.df1,out.df2[,c(2,3)],out.df3[,c(2,3)])
+names(out.df) <- c("time","x1","y1","x2","y2","x3", "y3")
+ggplot(data = out.df)+
+  geom_path(mapping=aes(x=x1,y=y1),color="darkred") +
+  geom_path(mapping=aes(x=x2,y=y2),color="red") +
+  geom_path(mapping=aes(x=x3,y=y3),color="orange") +
+  xlim(0,200) +
+  ylim(0,300) +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey", y = "Predator")
+
+#Let's increase alfa a bit more...
+
+state <- c(x=10,y=10) ## the initial population values for both x (prey) and y (predator) populations
+parameters <- c(alfa=1, ## alfa is the growth rate of the prey (x) population,
+                beta=0.02, ## beta is the effect of predator (y) on the prey (x) growth rate,
+                delta=0.02, ## gamma is the effect of prey (x) on the predator (y) growth rate
+                gamma=0.4)  ## and gamma is the predator (y) death rate.
+times <- seq(0,500,by=0.01) ##a sequence of time steps – uses function seq()
+out <- ode(y = state, times = times, func = LV, parms = parameters)
+out.df1 <- out.df
+out.df3 <- data.frame(out)
+out.df <- cbind(out.df1,out.df2[,c(2,3)],out.df3[,c(2,3)])
+names(out.df) <- c("time","x1","y1","x2","y2","x3", "y3")
+ggplot(data = out.df)+
+  geom_path(mapping=aes(x=x1,y=y1),color="darkred") +
+  geom_path(mapping=aes(x=x2,y=y2),color="red") +
+  geom_path(mapping=aes(x=x3,y=y3),color="orange") +
+  xlim(0,150) +
+  ylim(0,150) +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Prey", y = "Predator", title="Predator vs. Prey population (increasing alfa)")
+
 
